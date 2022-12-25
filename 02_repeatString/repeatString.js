56597627b3b162c99fc1679bf176e09d027fcadb
bhwk <120708160+bhwk@users.noreply.github.com>
2022-12-25 17:06:37 +0800

    Add solutions for exercise 01 - 03

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, timesRepeat) {
+    if (timesRepeat<0){
+        return 'ERROR';
+    }
+    
+    let totalString = "";
+    for (let i=0; i<timesRepeat; i++){
+        totalString+=string;
+    }
+    return totalString;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    i = string.length - 1;
+    let reversedString = "";
+    
+    while (reversedString.length < string.length){
+        reversedString += string.charAt(i);
+        i--;
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
