b80b33fb18cd35ec6c38df45cef78b49c7e234af
bhwk <120708160+bhwk@users.noreply.github.com>
2022-12-25 23:50:26 +0800

    Add solution for exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(firstNumber, secondNumber) {
+    let sum = 0;
+    switch(true){
+        case (typeof(firstNumber) != 'number' || typeof(secondNumber)!='number'):
+        case (firstNumber < 0 || secondNumber < 0):
+            return 'ERROR';
 
+        case (firstNumber < secondNumber):
+            for (let i = firstNumber; i<=secondNumber; i++){
+                sum+=i;
+            }
+            break;
+    
+        case (secondNumber < firstNumber):
+            for (i = secondNumber; i<=firstNumber; i++){
+                sum+=i;
+            }
+            break;
+    }
+    return sum;
 };
 
 // Do not edit below this line
