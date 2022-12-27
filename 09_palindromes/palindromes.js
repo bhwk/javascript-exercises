271a8145abcf0979741fbf1454933ebf15eb2f2e
bhwk <120708160+bhwk@users.noreply.github.com>
2022-12-27 21:34:08 +0800

    Add solution for exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    string = string.toLowerCase();
+    string = string.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"",).replace(/\s/g, '');
+    let reverseString = string.split("").reverse().join("");
+    return string === reverseString;
 };
 
 // Do not edit below this line
