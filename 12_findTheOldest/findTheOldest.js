9f3aa9edd799be87e17650206f134a5589c9a28c
bhwk <120708160+bhwk@users.noreply.github.com>
2022-12-27 22:16:11 +0800

    Add solution for exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let sortOldest = people.sort((beforePerson, afterPerson)=>{
+        if (!beforePerson.yearOfDeath) beforePerson.yearOfDeath = (new Date()).getFullYear();
+        if (!afterPerson.yearOfDeath) afterPerson.yearOfDeath = (new Date()).getFullYear();
+        let beforePersonAge= beforePerson.yearOfDeath -beforePerson.yearOfBirth;
+        let afterPersonAge= afterPerson.yearOfDeath -afterPerson.yearOfBirth;
 
+        if (beforePersonAge > afterPersonAge) return -1;
+        else return 1;
+    });
+    return sortOldest[0];
 };
 
 // Do not edit below this line
