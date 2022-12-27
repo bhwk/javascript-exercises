3ce0461c22b1ce61966f1ceb94e6cf44d9e3c622
bhwk <120708160+bhwk@users.noreply.github.com>
2022-12-27 21:52:34 +0800

    Add solution for exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,9 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+        if(index<0) return "OOPS";
+        else if (index < 3) return 1;
+        
+    // uses binet formula
+    return Math.round((1/Math.sqrt(5)) * (Math.pow(((1+Math.sqrt(5))/2),index) - Math.pow(((1-Math.sqrt(5))/2),index)));
 };
 
 // Do not edit below this line
